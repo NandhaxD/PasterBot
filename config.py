49a7bf85a9c9591caf77fa969a35fcb6951ec1a5
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-08-15 23:02:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os 
 
-API_ID = os.environ.get("API_ID", None)
-API_HASH = os.environ.get("API_HASH", None)
-BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-OWNER_ID = os.environ.get("OWNER_ID", None)
+API_ID = os.environ.get("API_ID", 7126006)
+API_HASH = os.environ.get("API_HASH", "f92b05be529835381859ead64a195fa2")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5635661417:AAGz5-T5RxV2Y-iOmoZ20n59t7XkCqVILyI")
+OWNER_ID = os.environ.get("OWNER_ID", 1491497760)
 HANDLER = ["~", ".","!","?","@","$"]
-support = os.environ.get("SUPPORT_CHAT", None)
-updates = os.environ.get("UPDATES_CHANNEL", None)
+support = os.environ.get("SUPPORT_CHAT", "nandhasupport")
+updates = os.environ.get("UPDATES_CHANNEL", "nandhabots")
