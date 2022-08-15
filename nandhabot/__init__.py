66f4a5690fbaf47f76b4223934c705921b786cb8
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-08-15 21:40:35 +0530

    Update __init__.py

--- nandhabot/__init__.py
+++ nandhabot/__init__.py
@@ -1,3 +1,4 @@
+import config
 from pyrogram import Client 
 
 
@@ -10,7 +11,7 @@ logging.basicConfig(
 
 bot = Client(
   "nandhabot", 
-  api_id=API_ID, 
-  api_hash=API_HASH, 
-  bot_token=BOT_TOKEN, 
+  api_id=config.API_ID, 
+  api_hash=config.API_HASH, 
+  bot_token=config.BOT_TOKEN, 
   plugins=dict(root="{}/modules".format(__name__)))
