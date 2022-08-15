07b8d75f271deaeeb36bb747d16def14abb68a21
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-08-15 21:33:39 +0530

    Create __init__.py

---
+++ nandhabot/__init__.py
@@ -0,0 +1,16 @@
+from pyrogram import Client 
+
+
+# enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[logging.FileHandler('logs.txt'),
+              logging.StreamHandler()],
+    level=logging.INFO)
+
+bot = Client(
+  "nandhabot", 
+  api_id=API_ID, 
+  api_hash=API_HASH, 
+  bot_token=BOT_TOKEN, 
+  plugins=dict(root="{}/modules".format(__name__)))
