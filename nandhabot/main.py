7103fe162c83d47d4aacd4956520e2b7608b5e22
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-08-15 21:47:05 +0530

    Create main.py

---
+++ nandhabot/main.py
@@ -0,0 +1,13 @@
+import config
+from nandhabot import bot
+
+
+if __name__ == "__main__":
+   bot.run
+   with bot:
+        bot.send_message(config.OWNER_ID, "Hey Don't worry iam online!")
+
+      
+      
+      
+      
