a78a71028cbb25e60ef6f82dc70eef94142fd93c
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-08-15 22:25:49 +0530

    Update and rename xxxx to start.py

---
+++ nandhabot/modules/start.py
@@ -0,0 +1,28 @@
+import config
+from nandhabot import bot 
+from pyrogram import filters 
+
+BANNED = [5555573352]
+
+START_TEXT = """ Welcome to Nandhas System!
+You can collect details about our network our bots
+thanks for visiting our bots!\n
+**YOUR INFORMATION:**
+Your name: {}
+Your ID: {}
+
+keep follow our network and check about for network/bots details!
+"""
+
+
+@bot.on_message(filters.command("start",config.HANDLER))
+async def start(_, message):
+    if message.from_user.id in BANNED:
+       await message.reply_text("sorry son you can't use bot")
+       return 
+    msg = message.reply_text("Welcome to Nandhas System! Please wait while we finish your information scan...")
+    msg.edit(START_TEXT,reply_markup=InlineKeyboardMarkup(
+                    [[InlineKeyboardButton("About our Details", callback_data="about"),
+                    ],[ InlineKeyboardButton("Our Group", url=f"t.me/{config.support}"),
+                       InlineKeyboardButton("Our Updates", url=f"t.me/{config.updates}")]]))
+                       
