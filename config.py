853e84495dea51ee714f72f26e752c6ca118d355
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-08-15 21:50:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ API_ID = os.environ.get("API_ID", None)
 API_HASH = os.environ.get("API_HASH", None)
 BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
 OWNER_ID = os.environ.get("OWNER_ID", None)
+HANDLER = ["~", ".","!","?","@","$"]
