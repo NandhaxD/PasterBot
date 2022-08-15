35d56bc1d5267bbf353d75ee4d4f1d394576ab56
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-08-15 22:40:23 +0530

    Update __main__.py

--- nandhabot/__main__.py
+++ nandhabot/__main__.py
@@ -4,8 +4,8 @@ from nandhabot import bot
 
 if __name__ == "__main__":
    bot.run
-   with bot:
-        bot.send_message(config.OWNER_ID, "Hey Don't worry iam online!")
+      with bot:
+      bot.send_message(config.OWNER_ID, "Hey Don't worry iam online!")
 
       
       
