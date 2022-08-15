19bfcde4108dafa4011015abe69eba999d445620
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-08-15 23:17:30 +0530

    Update start.py

--- nandhabot/modules/start.py
+++ nandhabot/modules/start.py
@@ -19,12 +19,13 @@ keep follow our network and check about for network/bots details!
 
 @bot.on_message(filters.command("start"))
 async def start(_, message):
+    USER = message.from_user
     if message.from_user.id in BANNED:
        await message.reply_text("sorry son you can't use bot")
        return 
     msg = await message.reply_text("Welcome to Nandhas System! Please wait while we finish your info scan...")
     time.sleep(2)
-    await msg.edit_text(START_TEXT,reply_markup=InlineKeyboardMarkup(
+    await msg.edit_text(START_TEXT.format(USER.mention, USER.id,reply_markup=InlineKeyboardMarkup(
                     [[InlineKeyboardButton("About our Details", callback_data="about"),
                     ],[ InlineKeyboardButton("Our Group", url=f"t.me/{config.support}"),
                        InlineKeyboardButton("Our Updates", url=f"t.me/{config.updates}")]]))
