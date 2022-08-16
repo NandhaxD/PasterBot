f1b70bdc089eaf9fc924ede5f6017771c16693bf
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-08-16 18:31:59 +0530

    Update banned.py

--- nandhabot/modules/banned.py
+++ nandhabot/modules/banned.py
@@ -20,7 +20,7 @@ async def banned(_, message):
                   USER = message.reply_to_message.from_user
                   text = message.text.split(" ")[1]
                   if not text:
-                         text = "No reason"
+                         text = "ɴᴏ ʀᴇᴀsᴏɴ"
                   else:
                          text = text
                   reason = text
