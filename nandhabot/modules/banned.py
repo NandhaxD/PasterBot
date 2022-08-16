3e04580577fababc2be18b182807a0b828f0f4c5
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-08-16 07:34:27 +0530

    Update banned.py

--- nandhabot/modules/banned.py
+++ nandhabot/modules/banned.py
@@ -1,8 +1,14 @@
 import config
 from pyrogram import filters
+from pyrogram.types import 
 from nandhabot import bot 
 from nandhabot.modules.start import BANNED 
 
+banned_text = """**Nandha System!**
+**Banned user:** {}
+**Banned ID:**  {}
+**Reason:** {}
+"""
 
 
 @bot.on_message(filters.command("addban"))
@@ -11,9 +17,16 @@ async def banned(_, message):
             if message.from_user.id in BANNED:
                  await message.reply_text("this son of bitch already banned!")
                  return 
-            elif message.reply_to_message:   
+            elif message.reply_to_message:
+                  USER = message.reply_to_message.from_user
+                  reason = message.text.split(" ")[1]   
                   BANNED.append(message.reply_to_message.from_user.id)
-                  await message.reply_text("checkout new banned user!")
+                  buttons = [[InlineKeyboardButton("Banned System!" , url="t.me/NandhaSystem")]]
+                  await message.reply_text("checkout new banned user!",
+                  reply_markup=InlineKeyboardMarkup(buttons))
+                  await bot.send_message("@NandhaSystem", banned_text.format(USER.mention,USER.id,reason),
+                  buttons = [[InlineKeyboardButton("ᴜɴʙᴀɴ" , callback_data="unban"),
+                                      InlineKeyboardButton("ᴜɴʙᴀɴ" , callback_data="delete")]]),reply_markup=InlineKeyboardMarkup(buttons)))
             else:
                    user_id = message.text.split(" ")[1]
                    if user_id in BANNED:
