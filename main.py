410bb1ffcb44ec5631bc8f861c9fa749a09d0c1b
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-09-02 21:32:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ bot = Client("pasterbot", api_id=config.API_ID, api_hash=config.API_HASH, bot_to
 
 @bot.on_message(filters.command("start"))
 async def start(_, message):
+         BOT_USERNAME = await bot.get_me().username
          await message.reply_text(f"""**Hello sir. {message.from_user.mention}**\n
 **The Paster Bot who can helps you to share code or share something whatever you can use this bot to past all Available Service.**
 
