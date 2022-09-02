ea625f24a58fb5b9bb2468bc48a30494deb9a7aa
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-09-02 21:16:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,13 +22,14 @@ bot = Client("pasterbot", api_id=config.API_ID, api_hash=config.API_HASH, bot_to
 
 @bot.on_message(filters.command("start"))
 async def start(_, message):
-         await message.reply_text(text=f"""**Hello sir, {message.from_user.mention}**
-**commands: /paste - reply to message text or  file text to Paste All  Available Servers.
-Made by @NandhaBots**
+         await message.reply_text("""**Hello sir. {message.from_user.mention}**\n
+**The Paster Bot who can helps you to share code or share something whatever you can use this bot to past all Available Service.**
+
+**~ /paste - command only works reply to (message/document)!
 """)
 
 
-bot.start()
+bot.run()
 
 
          
