75bc4bec9ee66b487adf7b7b2061643f43ba17ff
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-09-02 20:11:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import requests 
+import config
+
 from pyrogram import 
+from pyrogram import filters
+from pyrogram.types import ( 
+InlineKeyboardButton, 
+InlineKeyboardMarkup, )
+ 
+     
+
+bot = Client("pasterbot", api_id=config.API_ID, api_hash=config.API_HASH, bot_token=config.BOT_TOKEN)
+bot.start()
+
+
+@bot.on_message(filters.command("start"))
