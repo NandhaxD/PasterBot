6b84198543fdf9add260f52655d34b633b697fc0
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-09-02 21:38:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,12 +22,12 @@ bot = Client("pasterbot", api_id=config.API_ID, api_hash=config.API_HASH, bot_to
 
 @bot.on_message(filters.command("start"))
 async def start(_, message):
-         BOT_USERNAME = await bot.get_me().username
+         client = await bot.get_me()
          await message.reply_text(f"""**Hello sir. {message.from_user.mention}**\n
 **The Paster Bot who can helps you to share code or share something whatever you can use this bot to past all Available Service.**
 
 **~ /paste - command only works reply to (message/document) text format!**
-""",reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="SUBMIT ME", url=f"http://t.me/{BOT_USERNAME}?startgroup=true"),],[
+""",reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="SUBMIT ME", url=f"http://t.me/{client.username}?startgroup=true"),],[
 InlineKeyboardButton(text="Service Paste", callback_data="service")]]))
 
 
