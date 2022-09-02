bdaa3f21843985adb39a605dda7bd2f45d93b5e7
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-09-03 00:49:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import requests 
 import config
 import logging 
+from requests import post, get
+import os
+import aiofiles
+import socket
 
 from pyrogram import idle, Client 
 from pyrogram import filters
@@ -15,11 +19,47 @@ logging.basicConfig(
               logging.StreamHandler()],
     level=logging.INFO)
 
-     
+from SafoneAPI import SafoneAPI
+
+Safone = SafoneAPI()
 
 bot = Client("pasterbot", api_id=config.API_ID, api_hash=config.API_HASH, bot_token=config.BOT_TOKEN)
 
+async def send(url: str, *args, **kwargs):
+    async with session.post(url, *args, **kwargs) as resp:
+        try:
+            data = await resp.json()
+        except Exception:
+            data = await resp.text()
+    return data
 
+BASE = "https://batbin.me/"
+    
+      
+def spacebin(text):
+    url = "https://spaceb.in/api/v1/documents/"
+    res = post(url, data={"content": text, "extension": "txt"})
+    return f"https://spaceb.in/{res.json()['payload']['id']}"
+
+
+def _netcat(host, port, content):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    s.sendall(content.encode())
+    s.shutdown(socket.SHUT_WR)
+    while True:
+        data = s.recv(4096).decode("utf-8").strip("\n\x00")
+        if not data:
+            break
+        return data
+    s.close()
+    
+async def ezup(content):
+    loop = get_running_loop()
+    link = await loop.run_in_executor(
+        None, partial(_netcat, "ezup.dev", 9999, content)
+    )
+    return link
 
 @bot.on_message(filters.command("start"))
 async def start(_, message):
@@ -60,14 +100,46 @@ async def service(_, query):
 
 BATBIN_URL = "batbin.me/"
 
-@bot.on_message(filters.command(["paste","batbin"]))
-async def paste(_, message):
-          if message.command[0] == "b":
-              resp = await send(f"{BATBIN_URL}api/v2/paste", data=file_text)
-              code = resp["message"]
-              bat_link = f"{BATBIN_URL}{code}"
-              await message.reply_photo(bat_link,caption=f"**Successfully Pasted Batbin.me Service**: **Paster Link**:{batlink}")
-         
+@bot.on_message(filters.command('paste'))
+async def paste(_, m):
+ try:
+    reply = m.reply_to_message
+    if not reply:
+           await m.reply_text("Reply to Message or Text-File")
+    if reply.document:
+        doc = await m.reply_to_message.download()
+        async with aiofiles.open(doc, mode="r") as f:
+          file_text = await f.read()
+        os.remove(doc)
+        msg = await m.reply("**Starting to Past Process.**")
+        spacebin_url = spacebin(file_text)
+        safone_url = await Safone.paste(file_text)
+        ezup_link = await ezup(file_text)
+        resp = await send(f"{BASE}api/v2/paste", data=file_text)
+        code = resp["message"]
+        bat_link = f"{BASE}{code}"
+        await msg.edit("**Process Complete**")                  
+        caption = f"[SPACEBIN]({spacebin_url}) | [EZUP.DEV]({ezup_link})\n [SAFONE]({safone_url.link}) | [BATBIN]({bat_link})"
+        await m.reply_photo(photo=bat_link,caption=caption,
+                      reply_markup=InlineKeyboardMarkup(
+                          [[InlineKeyboardButton(text="BATBIN", url=bat_link),],[InlineKeyboardButton("SPACEBIN", url=spacebin_url),
+                         ],[ InlineKeyboardButton("EZUP.DEV", url=ezup_link),],[ InlineKeyboardButton(text="SAFONE", url=safone_url.link),]]))
+    elif reply.text or reply.caption:
+          text = reply.text or reply.caption
+          msg = await m.reply("**Starting to Past Process.**")                
+          spacebin_url = spacebin(text)
+          link = await ezup(text)
+          safone_url = await Safone.paste(text)
+          resp = await send(f"{BASE}api/v2/paste", data=text)
+          code = resp["message"]
+          bat_link = f"{BASE}{code}"
+          await msg.edit("**Process Complete.**")                 
+          caption = f"[SPACEBIN]({spacebin_url}) | [EZUP.DEV]({link})\n [SAFONE]({safone_url.link}) | [BATBIN]({bat_link}) "
+          await m.reply_photo(photo=bat_link,caption=caption,
+                      reply_markup=InlineKeyboardMarkup(
+                          [[InlineKeyboardButton(text="BATBIN", url=bat_link),],[InlineKeyboardButton(text="SAFONE", url=safone_url.link), ],[ InlineKeyboardButton("SPACEBIN", url=spacebin_url),
+                           ],[ InlineKeyboardButton("EZUP.DEV", url=link)]]))
+    
 
 bot.run()
 
