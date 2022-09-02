4978394e31927b6f059062df7198b0cdef4fcc55
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-09-02 20:32:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,4 +13,13 @@ bot = Client("pasterbot", api_id=config.API_ID, api_hash=config.API_HASH, bot_to
 bot.start()
 
 
+
 @bot.on_message(filters.command("start"))
+async def start(_, message):
+         await message.reply_text(f"""**Hello sir, {message.from_user.mention}**
+**commands: /paste - reply to message text or  file text to Paste All  Available Servers.
+
+Made by @NandhaBots**")
+
+
+         
